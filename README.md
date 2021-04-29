c4d87a388e1b54f0743b00222d254f55d39116a5
ababyduck <kassisaf@gmail.com>
2021-04-29 15:32:01 -0700

    Find target course in retrieved PDFs

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 TransferHero is a tool I wrote to help me find a community college that was offering Data Structures over the Summer.  Without it I may have had to wait another semester to transfer.
 
 ### How it works
+It doesn't (yet)!
 
 ### What I learned
 
-- Used Selenium to automate actions for a website that can't easily be parsed using simple requests due to literally everything being a div thanks to ng-bootstrap.
+- How to use Selenium to automate actions for a website that can't easily be parsed using simple requests due to literally everything being a div instead of a more sensible tag (thanks bootstrap!)
+- How to use the built-in csv library to handle parsing csv into a dictionary with key names based on what's in the header row
+- How to extract text from a PDF file in Python. I'm a little surprised to find that there's not a single ubiquitous library for this that's still maintained, but pymupdf seems to do alright.  Finding meaningful relationships to the data on the page is a challenge since the PDF format doesn't inherently structure data in natural reading order.  Would like to learn more about solving this problem.
+
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import csv
+import os
 import time
+from re import match
+
+import fitz  # AKA pymupdf
 from TransferHero import AssistOrg
 from TransferHero.Course import Course
 
@@ -8,6 +12,8 @@ INPUT_FILENAME = r'comp132.csv'
 SCHOOL_NAME = 'CSUMB'
 MAJOR_NAME = 'Computer Science'
 CATALOG_YEAR = '2020-2021'
+TARGET_COURSE = 'CST 238'
+
 
 if __name__ == '__main__':
     # Open the input csv and save the info we care about to a list of Course objects
@@ -18,17 +24,50 @@ if __name__ == '__main__':
             courses.append(Course(row['Institution'], row['Local Dept. Name & Number'], row['Local Course Title(s)']))
 
     # Download agreement PDFs for every community college in our list
-    print('Downloading course agreements...')
-
-    # Create the web driver we'll need for automation
-    driver = AssistOrg.create_driver(download_folder=DOWNLOAD_FOLDER)
-    for course in sorted(courses):
-        print(f'   {course}', end='')
-        AssistOrg.get_agreement_pdf(driver, CATALOG_YEAR, SCHOOL_NAME, course.school, MAJOR_NAME)
-        time.sleep(5)  # Avoid hammering
-    driver.close()
+    # print('Downloading course agreements...')
+    # Create the Selenium web driver we'll need for automation
+    # driver = AssistOrg.create_driver(download_folder=DOWNLOAD_FOLDER)
+    # for course in sorted(courses):
+    #     print(f'   {course}', end='')
+    #     AssistOrg.get_agreement_pdf(driver, CATALOG_YEAR, SCHOOL_NAME, course.school, MAJOR_NAME)
+    #     time.sleep(5)  # Avoid hammering
+    # driver.close()
 
     # TODO parse out our PDFs to verify which classes satisfy the class we need
+    for filename in os.listdir(DOWNLOAD_FOLDER):
+        absolute_filename = os.path.join(DOWNLOAD_FOLDER, filename)
+        file_ext = os.path.splitext(filename)
+
+        # Skip non-PDFs and remove duplicates
+        if file_ext[1].lower() != '.pdf':
+            continue
+        elif match(r'\(\d{1,2}\)', file_ext[0][-3:]):
+            os.remove(absolute_filename)
+            continue
+
+        # Open the PDF, look for the line containing our target course and find equivalencies around it
+        with fitz.open(absolute_filename) as pdf:
+            text = []
+            for page in pdf:
+                # Looks ugly but this gets rid of all the nasty zero-width spaces that prevent us from searching easily
+                text += page.get_text().encode('ascii', 'ignore').decode('unicode_escape').split('\n')
+
+            # Get school name before we look at courses. Looks like "From: {School Name}"
+            print(f'Parsing {filename}...', end='')
+            for line in text:
+                if "From: " in line:
+                    school = line.split(':')[1].strip()
+                    print(f'({school})')
+                    break
+
+            # See if our target course is listed
+            #  There may be multiple equivalent courses separated by '--- And ---' or '--- Or ---' but they may not
+            #  appear in natural reading order.  TODO: find a smarter way to extract all equivalent courses
+            #  https://pymupdf.readthedocs.io/en/latest/faq.html#how-to-extract-text-in-natural-reading-order
+            for line in text:
+                if TARGET_COURSE in line:
+                    print(f'   Found {TARGET_COURSE}')
+                    break
 
     # TODO Create a new spreadsheet of courses that have a transfer agreement
 
